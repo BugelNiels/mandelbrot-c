aa8292f9fb0269598ce10604ee845b69fcf3d178
Niels <bugel.niels@gmail.com>
2021-05-18 08:53:56 +0200

    added some unreadable code

---
+++ short/m.c
@@ -0,0 +1,5 @@
+                        #define F){for(
+double i,I,l,o,O,u,v=30,w=80,j=999;main(L F i=0;i++<v;F I=0;I++<w;F
+   l=o=O=u=0;u<4&l++<j;)u=O*O-o*o,o=2*O*o+i*2/v-1,O=u+I*3/w-2.5;
+    L=log(l)/log(j)*255,printf("\033[48;2;%d;%d;%dm ",L,L,L);}
+                puts("\033[40m"),usleep(20000);}}
